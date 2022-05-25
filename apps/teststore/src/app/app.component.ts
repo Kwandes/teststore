acc0e5c072d59bc06b9f6abd4d3c2d44b029ed09
Kwandes <bz.jasiek@gmail.com>
2022-05-25 20:37:55 +0200

    Add support for runtime env variables via env.js

--- apps/teststore/src/app/app.component.ts
+++ apps/teststore/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { IMessage } from '@interfaces';
+import { environment as env } from '../environments/environment';
 
 @Component({
   selector: 'teststore-root',
@@ -8,6 +9,6 @@ import { IMessage } from '@interfaces';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  hello$ = this.http.get<IMessage>('/api/hello');
+  hello$ = this.http.get<IMessage>(`${env.apiUrl}/api/hello`);
   constructor(private http: HttpClient) {}
 }
--- apps/teststore/src/environments/environment.prod.ts
+++ apps/teststore/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
-export const environment = {
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { IEnvironment } from './ienvironment';
+
+// production-specific environment
+export const environment: IEnvironment = {
   production: true,
+  envName: 'production',
+
+  apiUrl: (window as any)['env']
+    ? (window as any)['env']['apiUrl']
+    : 'http://localhost:3333',
 };
