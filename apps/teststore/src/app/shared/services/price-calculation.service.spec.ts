0ba71a44c88186a66e5ac98e06a5f9ff8c7e65ca
David <david.krtolica8@gmail.com>
2022-06-02 14:14:57 +0200

    Finished formatting the PriceCalcService tests

--- apps/teststore/src/app/shared/services/price-calculation.service.spec.ts
+++ apps/teststore/src/app/shared/services/price-calculation.service.spec.ts
@@ -1,5 +1,9 @@
 import { TestBed } from '@angular/core/testing';
-import { calculateDeliveryPriceParams, calculateSubtotalItems, calculateDiscounts } from './price-calculation-test-data.constant';
+import {
+  calculateDeliveryPriceParams,
+  calculateSubtotalItems,
+  calculateDiscounts,
+} from './price-calculation-test-data.constant';
 import { PriceCalculationService } from './price-calculation.service';
 
 describe('CalculatePriceService', () => {
@@ -11,38 +15,32 @@ describe('CalculatePriceService', () => {
   });
 
   // Parametrized test of the 'calculateDeliveryPrice()' method of PriceCalculationService
-  describe.each(calculateDeliveryPriceParams)(
-    'calculateDeliveryPrice()',
-    (params) => {
+  describe.each(calculateDeliveryPriceParams)('calculateDeliveryPrice()',(params) => {
       it(`Should correctly determine given delivery type price: ${params.deliveryType} = ${params.expectedPrice}`, () => {
-        expect(service.calculateDeliveryPrice(params.deliveryType)).toEqual(
-          params.expectedPrice
-        );
+        expect(service.calculateDeliveryPrice(params.deliveryType)).toEqual(params.expectedPrice);
       });
-    }
-  );
+  });
 
   // Parametrized test of the 'calculateSubtotal()' method of PriceCalculationService
-  describe.each(calculateSubtotalItems)(
-    'calculateSubtotal()',
-    (params) => {
-      it(`Should correctly determine given items: ${params.items} equal to subtotal = ${params.expectedPrice}`, () => {
-        expect(service.calculateSubtotal(params.items)).toEqual(
-          params.expectedPrice
-        );
-      });
-    }
-  );
+  describe.each(calculateSubtotalItems)('calculateSubtotal()', (params) => {
+    let itemListText = '';
+    params.items.forEach((item) => (itemListText += item.price + ', '));
+    it(`Should correctly determine given items with prices: ${itemListText} equal to subtotal = ${params.expectedPrice}`, () => {
+      expect(service.calculateSubtotal(params.items)).toEqual(params.expectedPrice);
+    });
+  });
 
   // Parametrized test of the 'calculateDiscount()' method of PriceCalculationService
-  describe.each(calculateDiscounts)(
-    'calculateDiscount()',
-    (params) => {
-      it(`Should correctly determine given discount with amount: ${params.discount.amount} equal to result = ${params.expectedReturnValue}`, () => {
-        expect(service.calculateDiscount(params.discount, params.subtotal)).toEqual(
-          params.expectedReturnValue
-        );
-      });
-    }
-  );
+  describe.each(calculateDiscounts)('calculateDiscount()', (params) => {
+    it(`Should correctly determine given discount with amount: ${params.discount.amount}
+      of type: ${params.discount.type}
+      and subtotal: ${params.subtotal} 
+      equal to saved amount: ${params.expectedReturnValue.savedAmount}
+      and total after discount: ${params.expectedReturnValue.discountedAmount}`, () => {
+      expect(
+        service.calculateDiscount(params.discount, params.subtotal)
+      ).toEqual(params.expectedReturnValue);
+    });
+  });
+
 });
