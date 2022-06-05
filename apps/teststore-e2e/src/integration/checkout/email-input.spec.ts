ad6b27d4adc1d03f8a5f8f70a4528a869c8e1185
Kwandes <bz.jasiek@gmail.com>
2022-06-05 04:09:11 +0200

    Slightly restructure checkout E2E UI tests

--- apps/teststore-e2e/src/integration/checkout/email-input.spec.ts
+++ apps/teststore-e2e/src/integration/checkout/email-input.spec.ts
@@ -1,22 +1,6 @@
-import * as products from '../fixtures/products.constant';
-
 describe('Checkout page E2E UI Tests', () => {
   beforeEach(() => {
-    //TO DO: Set up the
     cy.visit('/checkout');
-  });
-
-  describe('Checkout page load', () => {
-    it('should have the correct URL', () => {
-      cy.visit('/products');
-      cy.visit('/checkout');
-      cy.url().should('include', '/checkout');
-    });
-
-    it('should show the Checkout text', () => {
-      cy.url().should('include', '/checkout');
-      cy.get('[data-cy=header-checkout]').contains('Checkout');
-    });
   });
 
   describe('Your email input element', () => {
